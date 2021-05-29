47226dde3672907fa254b711bd0b6b0e1dc21232
firdese <58133981+firdese@users.noreply.github.com>
2021-05-29 23:19:40 +0800

    Update metal.py

--- metal.py
+++ metal.py
@@ -15,6 +15,229 @@ import matplotlib.pyplot as plt
 
 from utils import CvFpsCalc
 
+from tkinter import *
+import os
+from tkinter import filedialog
+
+#User Interface
+
+def register():
+    global register_screen
+    register_screen = Toplevel(main_screen)
+    register_screen.title("Register")
+    register_screen.geometry("920x180")
+ 
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+ 
+    Label(register_screen, text="Please enter details below", bg="blue").pack()
+    Label(register_screen, text="").pack()
+    username_lable = Label(register_screen, text="Username * ")
+    username_lable.pack()
+    username_entry = Entry(register_screen, textvariable=username)
+    username_entry.pack()
+    password_lable = Label(register_screen, text="Password * ")
+    password_lable.pack()
+    password_entry = Entry(register_screen, textvariable=password, show='*')
+    password_entry.pack()
+    Label(register_screen, text="").pack()
+    Button(register_screen, text="Register", width=10, height=1, bg="blue", command = register_user).pack()
+ 
+ 
+# Designing window for login 
+ 
+def login():
+    global login_screen
+    login_screen = Toplevel(main_screen)
+    login_screen.title("Login")
+
+    width= login_screen.winfo_screenwidth() 
+    height= login_screen.winfo_screenheight()
+
+    # Set window size
+    #login_screen.geometry("%dx%d" % (width, height))
+    login_screen.geometry('554x270')
+    login_screen.configure(bg = '#fbc95c')
+    Label(login_screen, text="Please enter details below to login", bg = '#fbc95c').pack()
+    Label(login_screen, text="", bg = '#fbc95c').pack()
+ 
+    global username_verify
+    global password_verify
+ 
+    username_verify = StringVar()
+    password_verify = StringVar()
+ 
+    global username_login_entry
+    global password_login_entry
+ 
+    Label(login_screen, text="Username * ", bg = '#fbc95c').pack()
+    username_login_entry = Entry(login_screen, textvariable=username_verify)
+    username_login_entry.pack()
+    Label(login_screen, text="", bg = '#fbc95c').pack()
+    Label(login_screen, text="Password * ", bg = '#fbc95c').pack()
+    password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+    password_login_entry.pack()
+    Label(login_screen, text="", bg = '#fbc95c').pack()
+    Button(login_screen, text="Login", width=10, height=1, bg = '#F9D179', command = login_verify).pack()
+ 
+# Implementing event on register button
+ 
+def register_user():
+ 
+    username_info = username.get()
+    password_info = password.get()
+ 
+    file = open(username_info, "w")
+    file.write(username_info + "\n")
+    file.write(password_info)
+    file.close()
+ 
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+ 
+    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+ 
+# Implementing event on login button 
+ 
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_login_entry.delete(0, END)
+    password_login_entry.delete(0, END)
+ 
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_sucess()
+        else:
+            password_not_recognised()
+ 
+    else:
+        user_not_found()
+ 
+# Designing popup for login success
+ 
+def login_sucess():
+    global login_success_screen
+    login_success_screen = Toplevel(login_screen)
+    login_success_screen.title("Success")
+    login_success_screen.geometry("200x100")
+    login_success_screen.configure(bg = '#fbc95c')
+    Label(login_success_screen, text="Login Success", bg = '#fbc95c').pack()
+    Button(login_success_screen, text="OK", bg = '#F9D179', command=delete_login_success).pack()
+ 
+# Designing popup for login invalid password
+ 
+def password_not_recognised():
+    global password_not_recog_screen
+    password_not_recog_screen = Toplevel(login_screen)
+    password_not_recog_screen.title("Success")
+    password_not_recog_screen.geometry("150x100")
+    Label(password_not_recog_screen, text="Invalid Password ").pack()
+    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+ 
+# Designing popup for user not found
+ 
+def user_not_found():
+    global user_not_found_screen
+    user_not_found_screen = Toplevel(login_screen)
+    user_not_found_screen.title("Success")
+    user_not_found_screen.geometry("150x100")
+    Label(user_not_found_screen, text="User Not Found").pack()
+    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
+ 
+# Deleting popups
+ 
+def delete_login_success():
+    main_screen.destroy()
+    browse_video_screen()
+ 
+def delete_browse_success():
+    browse_video_window.destroy() 
+ 
+def delete_password_not_recognised():
+    password_not_recog_screen.destroy()
+ 
+ 
+def delete_user_not_found_screen():
+    user_not_found_screen.destroy()
+ 
+# Designing Main(first) window 
+
+def logo_screen():
+    global logoScreen
+    logoScreen = Tk()
+    logoScreen.geometry('554x551')
+    logoScreen.title("Welcome")
+    bg = PhotoImage(file = "logo_METAL.png")
+    label1 = Label(logoScreen, image = bg)
+    label1.place(x = 0, y = 0)
+
+    logoScreen.after(5000, lambda: logoScreen.destroy())
+    logoScreen.mainloop()
+
+def main_account_screen():
+    global main_screen
+    main_screen = Tk()
+
+    #getting screen width and height of display
+    width= main_screen.winfo_screenwidth() 
+    height= main_screen.winfo_screenheight()
+
+    # Set window size
+    #main_screen.geometry("%dx%d" % (width, height))
+    main_screen.geometry('554x270')
+    main_screen.configure(bg = '#fbc95c')
+    main_screen.title("METAL")
+    Label(text="Select Your Choice", bg = '#fbc95c',width="3000", height="2", font=("Calibri", 13)).pack()
+    Label(text="", bg = '#fbc95c').pack()
+    Button(text="Login", height="2", bg = '#F9D179', width="15", command = login).pack()
+    Label(text="", bg = '#fbc95c').pack()
+    Button(text="Register", height="2", bg = '#F9D179', width="15", command=register).pack()
+ 
+    main_screen.mainloop()
+ 
+def browse_video_screen():
+
+    # Function for opening the
+    # file explorer window
+    def browseFiles():
+        global filename
+        filename = filedialog.askopenfilename(initialdir = "/",title = "Select a File",filetypes = (("Text files","*.txt*"),("all files","*.*")))
+        print("From Browse Screen Filename: " + filename)
+
+
+    global browse_video_window
+    # Create the root window
+    browse_video_window = Tk()
+
+    #getting screen width and height of display 
+    width = browse_video_window.winfo_screenwidth() 
+    height = browse_video_window.winfo_screenheight()
+    print(width)
+
+    # Set window size
+    browse_video_window.geometry("%dx%d" % (width, height))  
+
+    # Set window title
+    browse_video_window.title('File Explorer')
+
+    #Set window background color
+    browse_video_window.config(background = "white")
+  
+    # Create a File Explorer label
+    label_file_explorer = Label(browse_video_window,text = "Select Video Files",width = 100, height = 4,fg = "blue").pack()    
+    button_explore = Button(browse_video_window,text = "Browse Video",command = browseFiles).pack()
+    button_process = Button(browse_video_window,text = "Start Process",command = delete_browse_success).pack()
+
+    # Let the window wait for any events
+    browse_video_window.mainloop()
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -40,8 +263,10 @@ def get_args():
     return args
 
 
-def main():
+def main(filename):
     args = get_args()
+    
+    print("Filename in main is " + filename)
 
     cap_device = args.device
     cap_width = args.width
@@ -53,7 +278,7 @@ def main():
 
     use_brect = args.use_brect
 
-    cap = cv.VideoCapture('sample_videos/compare.MOV')   
+    cap = cv.VideoCapture(filename)   
     cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
     length = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
@@ -533,4 +758,9 @@ def calculate_Angle(image,joint_number,joint_name,display_height,x1,y1,x2,y2,x3,
   return angle_interest
 
 if __name__ == '__main__':
-    main()
+
+    filename = ''
+    logo_screen()
+    main_account_screen()
+    print("Filename from after main screen destroyed is: " + filename)
+    main(filename)
